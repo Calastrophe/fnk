7e9e0063bf6961d549f770d29bb36023254e68e9
Calastrophe <zediant@gmail.com>
2023-10-12 11:09:54 -0500

    new db layout, rid of ds store

--- backend/migrations/20231012022525_tests.up.sql
+++ backend/migrations/20231012022525_tests.up.sql
@@ -6,7 +6,7 @@ CREATE TABLE
     "tests" (
         id UUID NOT NULL PRIMARY KEY DEFAULT (uuid_generate_v4()),
         teacher UUID NOT NULL,
-        name VARCHAR(100),
+        name VARCHAR(100) NOT NULL,
 
         FOREIGN KEY (teacher) REFERENCES teachers(id)
     );
@@ -15,8 +15,10 @@ CREATE TABLE
     "testresults" (
         id UUID NOT NULL PRIMARY KEY DEFAULT (uuid_generate_v4()),
         test UUID NOT NULL,
-        name VARCHAR(100),
-        score INT,
+        name VARCHAR(100) NOT NULL,
+        score INT NOT NULL,
+        status INT NOT NULL,
+        flagged BIT NOT NULL,
 
         FOREIGN KEY (test) REFERENCES tests(id)
     );
