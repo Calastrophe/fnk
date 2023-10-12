4f15a6785ad34f7233cbfe9fa51175e3eb6674c2
JGPadilla9 <jgpadilla09@gmail.com>
2023-10-12 10:48:55 -0500

    Login and bootstrap added Sorry it took me so long to make this pull request but these are the changes I have made so far.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -9,12 +9,15 @@ enum Route {
     Home,
     #[at("/hello-server")]
     HelloServer,
+    #[at("/login")]
+    Login,
 }
 
 fn switch(routes: Route) -> Html {
     match routes {
         Route::Home => html! { <h1>{ "Hello Frontend" }</h1> },
         Route::HelloServer => html! { <HelloServer /> },
+        Route::Login => html! { <Login /> },
     }
 }
 
@@ -73,6 +76,50 @@ fn hello_server() -> Html {
                 <div>{"Error requesting data from server: "}{err}</div>
             }
         }
+    }
+}
+
+#[function_component(Login)]
+fn login() -> Html
+{
+    html! 
+    {
+        <>
+            <div>
+                <div class="row m-2">
+                    <div class="col">
+                        <h3 class="text-center">{"Login"}</h3>
+                        <h1 class="text-center">{"FNK"}</h1>
+                    </div>
+                </div>
+                <form>
+                    <div class="mb-3 row">
+                        <div class="col-3"></div>
+                        <div class="col-6">
+                            <label for="username" class="form-label">{"Username"}</label>
+                            <input class="form-control" id="username" type="text" aria-describedy="username-input" />
+                            <div id="username-input" class="form-text">{"username you made"}</div>
+                        </div>
+                        <div class="col-3"></div>
+                    </div>
+                    <div class="mb-3 row">
+                        <div class="col-3"></div>
+                        <div class="col-6">
+                            <label for="password" class="form-label">{"Password"}</label>
+                            <input id="password" type="password" class="form-control" />
+                        </div>
+                        <div class="col-3"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-3"></div>
+                        <div class="col-6">
+                            <button type="submit" class="btn btn-primary">{"Login"}</button>
+                        </div>
+                        <div class="col-3"></div>
+                    </div>
+                </form>
+            </div>
+        </>
     }
 }
 
