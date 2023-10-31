9056ee9ddb21871dc3a8419a980ab7a4e7c787ee
JGPadilla9 <jgpadilla09@gmail.com>
2023-10-31 14:05:14 -0500

    Remove .DS_Store from everywhere

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -4,107 +4,9 @@ use std::rc::Rc;
 use wasm_bindgen::closure::Closure;
 use wasm_bindgen::JsCast;
 use wasm_bindgen::JsValue;
-//use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
-//use web_sys::window;
-//use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 use yew::prelude::*;
 use yew_router::prelude::*;
-
-// const MAX_WIDTH: u32 = 800;
-// const MAX_HEIGHT: u32 = 400;
-
-// #[function_component(DrawTest)]
-// fn app_model() -> Html {
-//     let MAX_WIDTH_str = MAX_WIDTH.to_string();
-//     let MAX_HEIGHT_str = MAX_HEIGHT.to_string();
-
-//     html! {
-//         <>
-//             <svg
-//                 width = {MAX_WIDTH_str}
-//                 height = {MAX_HEIGHT_str}
-//                 version="1.1"
-//                 xmlns="http://www.w3.org/2000/svg"
-//             >
-//                 {draw_path()}
-//             </svg>
-//         </>
-//     }
-// }
-
-// fn draw_path(path: &String) -> Html {
-//     let draw_path = if !path.is_empty() {
-//         html! {
-//             <path d = {path.clone()} stroke="blue" fill="none" />
-//         }
-//     } else {
-//         html! {}
-//     };
-
-//     draw_path
-// }
-
-// pub struct AppModel2 {
-//     link: ComponentLink<Self>,
-//     mouse_down: bool,
-//     path: String,
-//     cx: f64,
-//     cy: f64,
-// }
-
-// pub enum Msg {
-//     StartDrawing,
-//     ContinueDrawing(f64, f64),
-//     StopDrawing,
-//     UpdateView,
-// }
-
-// impl Component for DrawTest {
-//     type Message = Msg;
-//     type Properties = ();
-
-//     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-//         AppModel2 {
-//             link,
-//             mouse_down: false,
-//             path: String::from(""),
-//             cx: 0.0,
-//             cy: 0.0,
-//         }
-//     }
-
-//     fn update(&mut self, msg: Self::Message) -> ShouldRender {
-//         match msg {
-//             Msg::StartDrawing => {
-//                 self.mouse_down = true;
-//                 self.path = format!("M{} {}", self.cx, self.cy);
-//             }
-//             Msg::ContinueDrawing(x, y) => {
-//                 if self.mouse_down {
-//                     self.path.push_str(&format!(" L{} {}", x, y));
-//                     self.cx = x;
-//                     self.cy = y;
-//                 }
-//             }
-//             Msg::StopDrawing => {
-//                 self.mouse_down = false;
-//             }
-//             Msg::UpdateView => {
-//                 // Do something when updating the view, if needed
-//             }
-//         }
-//         true
-//     }
-
-//     fn changed(&mut self, _props: Self::Properties) -> ShouldRender {
-//         false
-//     }
-
-//     fn view(&self) -> Html {
-//         draw_path(&self.path)
-//     }
-// }
 
 #[derive(Clone, Routable, PartialEq)]
 enum Route {
@@ -227,60 +129,15 @@ fn login() -> Html {
     }
 }
 
-// #[function_component(DrawTest)]
-// fn drawtest() -> Html {
-//     let canvas: HtmlCanvasElement = web_sys::window()
-//         .unwrap()
-//         .document()
-//         .unwrap()
-//         .get_element_by_id("myCanvas")
-//         .unwrap()
-//         .unchecked_into();
+fn clear_canvas(canvas: &web_sys::HtmlCanvasElement) -> Result<(), JsValue> {
+    let context = canvas
+        .get_context("2d")?
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
 
-//     // Get the 2D rendering context
-//     let ctx: CanvasRenderingContext2d = canvas
-//         .get_context("2d")
-//         .unwrap()
-//         .unwrap()
-//         .dyn_into()
-//         .unwrap();
-
-//     // Set canvas dimensions
-//     let canvas_offset_x = canvas.offset_left();
-//     let canvas_offset_y = canvas.offset_top();
-//     canvas.set_width(window().unwrap().inner_width().into() - canvas_offset_x);
-//     canvas.set_height(window().unwrap().inner_height().into() - canvas_offset_y);
-
-//     let is_painting = Rc::new(RefCell::new(false));
-//     let line_width = Rc::new(RefCell::new(5));
-//     let start_x = Rc::new(RefCell::new(0));
-//     let start_y = Rc::new(RefCell::new(0));
-
-//     let closure = Closure::wrap(Box::new(|| {
-//         e.prevent_default();
-
-//         let is_painting = is_painting.borrow().clone();
-//         let line_width = line_width.borrow().clone();
-
-//         if is_painting {
-//             ctx.begin_path();
-//             ctx.set_line_width(line_width as f64);
-//             ctx.set_line_cap("round");
-//             ctx.line_to(
-//                 e.client_x() as f64 - canvas_offset_x as f64,
-//                 e.client_y() as f64 - canvas_offset_y as f64,
-//             );
-//             ctx.stroke();
-//         }
-//     }));
-
-//     canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref());
-//     closure.forget();
-
-//     html! {
-//         <canvas id="myCanvas" width="200" height="100" style="border:1px solid #000000;"></canvas>
-//     }
-// }
+    context.clear_rect(0.0, 0.0, canvas.width() as f64, canvas.height() as f64);
+    Ok(())
+}
 
 fn start() -> Result<(), JsValue> {
     let document = web_sys::window().unwrap().document().unwrap();
@@ -290,7 +147,7 @@ fn start() -> Result<(), JsValue> {
     document.body().unwrap().append_child(&canvas)?;
     canvas.set_width(640);
     canvas.set_height(480);
-    canvas.style().set_property("border", "solid")?;
+    canvas.set_attribute("style", "border: solid; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); max-width: 100%;")?;
     let context = canvas
         .get_context("2d")?
         .unwrap()
@@ -332,6 +189,18 @@ fn start() -> Result<(), JsValue> {
         closure.forget();
     }
 
+    let clear_button = document.create_element("button")?;
+    clear_button.set_text_content(Some("Clear Canvas"));
+
+    let canvas_clone = canvas.clone();
+    let closure = Closure::<dyn FnMut(_)>::new(move |_event: web_sys::MouseEvent| {
+        clear_canvas(&canvas_clone).expect("Failed to clear the canvas");
+    });
+    clear_button.add_event_listener_with_callback("click", closure.as_ref().unchecked_ref())?;
+    closure.forget();
+
+    document.body().unwrap().append_child(&clear_button)?;
+
     Ok(())
 }
 
@@ -342,14 +211,20 @@ fn drawtest() -> Html {
     html! {
         <>
             <div>
-                <canvas id="myCanvas" width="200" height="100" style="border:1px solid #000000;"></canvas>
+                <div class="row m-2">
+                    <div class="col">
+                        <h3 class="text-center">{"Draw Test"}</h3>
+                        <h1 class="text-center">{"FNK"}</h1>
+                    </div>
+                </div>
             </div>
+            <canvas width="200" height="100"></canvas>
         </>
     }
 }
 
-
 fn main() {
     wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
     console_error_panic_hook::set_once();
+    yew::Renderer::<App>::new().render();
 }
